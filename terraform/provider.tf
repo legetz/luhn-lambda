f44371d6aed401225d0a0c82fea5060cd32b4412
Leo Jokinen <leo.jokinen81@gmail.com>
2021-10-26 20:45:29 +0300

    Add providers minimum versions

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.63.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = "~> 1.0.9"
+}
+
 provider "aws" {
-  profile     = "${var.aws_profile}"
-  region      = "${var.aws_region}"
+  profile     = var.aws_profile
+  region      = var.aws_region
   max_retries = 1
 }
