e136ffe8b04d18902d6cf25b679350ba8189b76d
Leo Jokinen <leo.jokinen81@gmail.com>
2021-10-26 23:17:43 +0300

    Added generate logic

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -15,17 +15,57 @@ const response = ({ statusCode = 200, headers = defaultHeaders, body }: Response
 const handler = (event, context, callback): void => {
   if (event) {
     console.log('Incoming event: ', event);
-  }
-  const newLuhn = luhn.generate('4454066197024125');
-  console.log(`Generated ${newLuhn}`);
+    switch (event.path) {
+      case '/generate':
+        if (event.httpMethod === 'GET' && event.queryStringParameters?.startNumber) {
+          const amount = parseInt(event.queryStringParameters.amount, 10) || 1;
+          if (amount > 100000) {
+            const resp = {
+              statusCode: 400,
+              body: JSON.stringify({
+                errorMessage: 'amount must be under 100000',
+              }),
+            };
+            callback(null, response(resp));
+            return;
+          }
+          const startNum = parseInt(event.queryStringParameters.startNumber, 10);
 
-  const resp = {
-    body: JSON.stringify({
-      success: true,
-      luhn: newLuhn,
-    }),
-  };
-  callback(null, response(resp));
+          const resultList = [];
+          let nextNum = startNum;
+          for (let i = 0; i < amount; i++) {
+            resultList.push(luhn.generate(nextNum.toString()));
+            nextNum++;
+          }
+
+          const resp = {
+            body: JSON.stringify({
+              amount,
+              results: resultList,
+            }),
+          };
+          callback(null, response(resp));
+        } else {
+          const resp = {
+            statusCode: 400,
+            body: JSON.stringify({
+              errorMessage: 'Specify startNumber and use HTTP GET',
+            }),
+          };
+          callback(null, response(resp));
+        }
+        return;
+      default:
+        break;
+    }
+  }
+  callback(
+    null,
+    response({
+      statusCode: 404,
+      body: null,
+    })
+  );
 };
 
 export { handler };
